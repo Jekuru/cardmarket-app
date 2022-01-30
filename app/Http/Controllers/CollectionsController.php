6685493709997c1b6fe079bccb6973932e6e97ff
Jekuru <FJOAndreu@hotmail.es>
2022-01-30 15:21:48 +0100

    Añadida la opcion de registrar coleccion con una carta existente. Si todo sale bien después del testeo, este es el último commit.

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -18,37 +18,45 @@ class CollectionsController extends Controller
         // JSON
         $data = $req->getContent();
         $data = json_decode($data);
-       
-        // NUEVA COLECCION
-        $collection = new Collection();
-        // NUEVA CARTA
-        $card = new Card();
+
         try {
-            // COMPROBAR SI LA COLECCION Y CARTA YA HAN SIDO REGISTRADAS
+            // NUEVA COLECCION
+            $collection = new Collection();
+
+            // Comprobar si la coleccion ya ha sido registrada con ese nombre
             $collectionExists = Collection::where('name', '=', $data->name)->first();
-            $cardExists = Card::where('name', '=', $data->card_name)->first();
-            
-            if(!$collectionExists && !$cardExists){
-                // Asignar valores del JSON a la nueva colección
+
+            // Variable para conocer si se utilizara una carta existente o una nueva
+            $existingCard = false;
+
+            // !! NUEVA COLECCION CON CARTA EXISTENTE !!
+            if (isset($data->card_id) && !$collectionExists){
+                // Comprobar si la carta existe
+                $cardExists = Card::where('id', '=', $data->card_id)->first();
+                $existingCard = true;
+            }
+            // !! NUEVA COLECCIÓN CON NUEVA CARTA !!
+            else if (!isset($data->card_id) && !$collectionExists){
+                // NUEVA CARTA
+                $card = new Card();
+                // Comprobar si la nueva carta ya ha sido registrada
+                $cardExists = Card::where('name', '=', $data->card_name)->first();
+                if(!$cardExists){ // Asignar valores del JSON a la primera nueva carta de la colección
+                    $card->name = $data->card_name;
+                    $card->description = $data->card_description;
+                    $card->save();
+                }
+            }
+
+            // CREAR COLECCION
+            if(!$collectionExists){ // Asignar valores del JSON a la nueva colección
                 $collection->name = $data->name;
                 $collection->symbol = $data->symbol;
                 $collection->edition_date = $data->edition_date;
-                // Asignar valores del JSON a la primera nueva carta de la colección
-                $card->name = $data->card_name;
-                $card->description = $data->card_description;
-                
-            } if($collectionExists){
-                $msg['status'] = 0;
-                $msg['msg'] = "No se pudo dar de alta la coleccion especificada, la coleccion ".$data->name." ya existe.";
-            } else if($cardExists){
-                $msg['status'] = 0;
-                $msg['msg'] = "No se pudo dar de alta la carta especificada, la carta ".$data->card_name." ya existe.";
-            }
-            if(!$collectionExists && !$cardExists){
                 $collection->save();
-                $card->save();
-                $registeredCollection = Collection::where('name', '=', $collection->name)->first();
-                $registeredCard = Card::where('name', '=', $card->name)->first();
+            }
+            // Añadir nueva carta a la nueva coleccion
+            if(!$existingCard){
                 DB::table('cards_collections')->insert([
                     'card_id' => $card->id,
                     'collection_id' => $collection->id,
@@ -57,6 +65,23 @@ class CollectionsController extends Controller
                 ]);
                 $msg['status'] = 1;
                 $msg['msg'] = "Coleccion ".$collection->name." y carta " .$card->name. " registradas correctamente";
+            } else if ($existingCard){
+                DB::table('cards_collections')->insert([
+                    'card_id' => $cardExists->id,
+                    'collection_id' => $collection->id,
+                    'created_at' => \Carbon\Carbon::now(),
+                    "updated_at" => \Carbon\Carbon::now()
+                ]);
+                $msg['status'] = 1;
+                $msg['msg'] = "Coleccion ".$collection->name." registrada correctamente";
+            }
+
+            if($collectionExists){
+                $msg['status'] = 0;
+                $msg['msg'] = "No se pudo dar de alta la coleccion especificada, la coleccion ".$data->name." ya existe.";
+            } else if($cardExists && !$existingCard){
+                $msg['status'] = 0;
+                $msg['msg'] = "No se pudo dar de alta la carta especificada, la carta ".$data->card_name." ya existe.";
             }
         } catch(\Exception $e){
             $msg['status'] = 0;
